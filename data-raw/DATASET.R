a30176146c6f96c5fd3d565dc5a7ed8b6ae2d6d5
TessGold <tessgoldmann@aol.com>
2022-03-29 22:17:39 -0400

    the worst code i have ever written but the hex codes are in the dataset now

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,5 +1,23 @@
 ## code to prepare `DATASET` dataset goes here
 library(tidyverse)
-goghColors <- readr::read_csv("data-raw/color_space.csv")
+hexdigit<-c(0:9,letters[1:6])
+toRGB <- function(rgb) {
+  return(paste("#", hexdigit[as.numeric(stringr::str_split(stringr::str_remove_all(
+    rgb,"[( )]"), "\\,")[[1]])%/%16+1][1],
+    hexdigit[as.numeric(stringr::str_split(stringr::str_remove_all(
+      rgb,"[( )]"), "\\,")[[1]])%%16+1][1],
+    hexdigit[as.numeric(stringr::str_split(stringr::str_remove_all(
+      rgb,"[( )]"), "\\,")[[1]])%/%16+1][2],
+    hexdigit[as.numeric(stringr::str_split(stringr::str_remove_all(
+      rgb,"[( )]"), "\\,")[[1]])%%16+1][2],
+    hexdigit[as.numeric(stringr::str_split(stringr::str_remove_all(
+      rgb,"[( )]"), "\\,")[[1]])%/%16+1][3],
+    hexdigit[as.numeric(stringr::str_split(stringr::str_remove_all(
+      rgb,"[( )]"), "\\,")[[1]])%%16+1][3], sep = ""))
+}
+goghColors <- readr::read_csv("data-raw/color_space.csv") %>%
+  janitor::clean_names() %>%
+  mutate(hex = map(rgb, toRGB))
 
 usethis::use_data(goghColors, overwrite = TRUE)
+
