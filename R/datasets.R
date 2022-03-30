fc197723fa202a16854a76c3d3962115d7aa25cf
TessGold <tessgoldmann@aol.com>
2022-03-30 11:58:28 -0400

    added descrition and also some dependancies I hope

---
+++ R/datasets.R
@@ -0,0 +1,13 @@
+#' Sampling of Colors from Van Gogh Paintings
+#'
+#'
+#' @format A data frame of 986 rows representing colors selected from
+#' Van Gogh's painting. 5 colors were selected from each painting.
+#' Consists of 3 variables.
+#'
+#' \describe{
+#'   \item{name}{Color name}
+#'   \item{rgb}{Color RGB value}
+#'   \item{hex}{Color hex code value}
+#' }
+"goghColors"
