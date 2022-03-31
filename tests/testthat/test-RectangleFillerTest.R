9cd7aeefd0f350ecda93fe4805bac725cb96e05f
Katelyn Diaz <888knd@gmail.com>
2022-03-30 21:31:57 -0400

    test currently fails but idk why

--- tests/testthat/test-RectangleFillerTest.R
+++ tests/testthat/test-RectangleFillerTest.R
@@ -1,3 +1,10 @@
+library(tidyverse)
+library(testthat)
+plotOne <- ggplot(starwars, aes(x = mass, y = height)) +
+  geom_point() +
+  scale_x_continuous()
+
 test_that("Rectange Filler works", {
-  expect_equal(2 * 2, 4)
+  expect_equal(RectangleFiller(plotOne, "#D4F090", "green", "#90CFF0",
+                             num_rect = 3, x_min = -1, x_max = 26), 9)
 })
