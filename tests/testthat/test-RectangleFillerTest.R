3c4d474aea3003980c24b87b22bb82a359e778dd
Katelyn Diaz <888knd@gmail.com>
2022-04-01 09:03:12 -0400

    fixed warning about `hello()` func not being documented

--- tests/testthat/test-RectangleFillerTest.R
+++ tests/testthat/test-RectangleFillerTest.R
@@ -1,13 +1,12 @@
 # library(tidyverse)
 # library(testthat)
-library(dplyr)
+#library(dplyr)
 library(ggplot2)
 plotOne <- ggplot(starwars, aes(x = mass, y = height)) +
   geom_point() +
   scale_x_continuous()
 
 test_that("Rectange Filler works", {
-  expect_equal(RectangleFiller(plotOne, "#D4F090", "green", "#90CFF0",
-                             num_rect = 3, x_min = -1, x_max = 26), 9)
+  expect_equal(RectangleFiller(plotOne, c("red", "blue", "green", "orange", "purple")))
 })
 #str.object
