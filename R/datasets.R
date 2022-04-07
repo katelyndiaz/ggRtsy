4865abe5a874ba6080322d8dd1b6c3d5b7e20ecb
TessGold <tessgoldmann@aol.com>
2022-04-07 11:35:18 -0400

    added documentation for new dataset

--- R/datasets.R
+++ R/datasets.R
@@ -14,3 +14,24 @@
 #' @source Kaggle Konstantinos Katserelis <https://www.kaggle.com/datasets/pointblanc/colors-of-van-gogh?select=color_space.csv>
 #' Note data is released under CC BY-SA 4.0
 "goghColors"
+
+
+#' Van Gogh Paintings Information
+#'
+#' This dataset consists of information about and colors from all 1931
+#' of Van Gogh's paintings.
+#'
+#' @format A data frame of 1931 rows representing information from
+#' Van Gogh's paintings. Consists of 6 variables.
+#'
+#' \describe{
+#'   \item{name}{Painting name}
+#'   \item{colors}{5 colors picked from the painting}
+#'   \item{year}{Year painting was painted}
+#'   \item{genre}{Genre of painting}
+#'   \item{style}{Style of painting}
+#'   \item{link}{Link of image of painting}
+#' }
+#' @source Kaggle Konstantinos Katserelis <https://www.kaggle.com/datasets/pointblanc/colors-of-van-gogh?select=color_space.csv>
+#' Note data is released under CC BY-SA 4.0
+"goghPaintingSets"
