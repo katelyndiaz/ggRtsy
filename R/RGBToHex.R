08fa734ccd655a357e49a923380d8af0fba65d50
TessGold <tessgoldmann@aol.com>
2022-04-14 11:36:42 -0400

    added function RGBtoHex and tests

---
+++ R/RGBToHex.R
@@ -0,0 +1,40 @@
+#' @title RGBToHex
+#'
+#' Converts RGB colors to Hex code colors
+#'
+#' Taking input of a vector of RGB values in the format
+#' "(0, 72, 186)", etc, and returns the corresponding
+#' hex value in the format "#0048ba"
+#'
+#' @param rgbVec a vector of strings in the format "(0, 72, 186)"
+#'
+#' @return vector of hex code strings in the format "#0048ba"
+#'
+#' @export
+#'
+#' @importFrom magrittr %>%
+NULL
+
+
+rgbToHex <- function(rgbVec, ...) {
+
+  hexdigit<-c(0:9,letters[1:6])
+  rgbMapFunc <- function(rgb) {
+    return(paste("#", hexdigit[as.numeric(stringr::str_split(stringr::str_remove_all(
+      rgb,"[( )]"), "\\,")[[1]])%/%16+1][1],
+      hexdigit[as.numeric(stringr::str_split(stringr::str_remove_all(
+        rgb,"[( )]"), "\\,")[[1]])%%16+1][1],
+      hexdigit[as.numeric(stringr::str_split(stringr::str_remove_all(
+        rgb,"[( )]"), "\\,")[[1]])%/%16+1][2],
+      hexdigit[as.numeric(stringr::str_split(stringr::str_remove_all(
+        rgb,"[( )]"), "\\,")[[1]])%%16+1][2],
+      hexdigit[as.numeric(stringr::str_split(stringr::str_remove_all(
+        rgb,"[( )]"), "\\,")[[1]])%/%16+1][3],
+      hexdigit[as.numeric(stringr::str_split(stringr::str_remove_all(
+        rgb,"[( )]"), "\\,")[[1]])%%16+1][3], sep = ""))
+  }
+
+  return(purrr::map_chr(rgbVec, rgbMapFunc))
+
+}
+
---
+++ tests/testthat/test-RGBToHex.R
@@ -0,0 +1,4 @@
+test_that("RGBToHex function works", {
+  expect_equal(rgbToHex("(0, 72, 186)"),"#0048ba")
+  expect_equal(rgbToHex(c("(119, 168, 99)", "(211,77,226)")),c("#77a863", "#d34de2"))
+})
