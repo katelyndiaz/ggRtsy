f1a317e98b1e197e20fb1c0cb81b6705aabf518a
Katelyn Diaz <888knd@gmail.com>
2023-09-12 15:13:18 -0400

    \value added to `gogh_interpolate.R`

--- R/PaletteColors.R
+++ R/PaletteColors.R
@@ -12,14 +12,16 @@ gogh_palettes_pop <- list(
   `irises` = c("#415AA6", "#6382BF", "#65A67C", "#8CA653", "#A66D58"), # https://uploads0.wikiart.org/00213/images/vincent-van-gogh/antique-3840759.jpg!Large.jpg
   `almondBlossoms` = c("#236E8C", "#20788C", "#49B3BF", "#41BFBF", "#BDBF75") # https://uploads6.wikiart.org/images/vincent-van-gogh/branches-with-almond-blossom-1890(1).jpg!Large.jpg
 )
+
 #' Interpolate the color palette
 #' @param palette the ggRtsy painting color palette of choice
-#' @param reverse Boolean, will be TRUE if user wants the palette reversed
+#' @param reverse Boolean, will be TRUE if the user wants the palette reversed
 #' @param ... further arguments passed to [ggRtsy::gogh_interpolate()]
 #' @export
+#' @return A color interpolation function.
+#'   If `reverse` is TRUE, it returns a reversed color interpolation function.
 gogh_interpolate <- function(palette = "starryNight", reverse = FALSE, ...) {
   pal <- gogh_palettes_pop[[palette]]
-  # pal <- palette
   if (reverse == TRUE) {
     pal <- rev(pal)
   }
@@ -28,46 +30,48 @@ gogh_interpolate <- function(palette = "starryNight", reverse = FALSE, ...) {
 
 #' Creating the color scale for Van Gogh Colors
 #' @param palette the ggRtsy painting color palette of choice
-#' @param reverse Boolean, will be TRUE if user wants the palette reversed
+#' @param reverse Boolean, will be TRUE if the user wants the palette reversed
 #' @param discrete Boolean if color aesthetic is discrete
 #' @param ... further arguments passed to [ggRtsy::scale_color_gogh()]
-#' @examples
-#' if (require(ggplot2)) {
-#'   data <- data.frame(c = LETTERS[1:3],x = c(1,5,7),y = c(5,9,13))
-#'   ggplot(data, aes(x,y,color = c))+geom_point()+scale_color_gogh()
-#' }
 #' @export
+#' @return A ggplot2 color scale.
+#'   If `discrete` is TRUE, it returns a discrete color scale; otherwise, a continuous color scale.
 scale_color_gogh <- function(palette = "starryNight",
                              discrete = TRUE, reverse = FALSE, ...) {
   pal <- gogh_interpolate(palette, reverse)
 
   if (discrete == TRUE) {
-    # ggplot2::discrete_scale("color", palette, palette = pal, ...)
     ggplot2::discrete_scale("colour", paste0("gogh_", palette), palette = pal, ...)
   } else {
-    ggplot2::scale_colour_gradientn(colours = pal(256), ...) # gradient is the default for continuous
+    ggplot2::scale_colour_gradientn(colours = pal(256), ...)
   }
 }
+
 #' Creating the color fill scale for Van Gogh Colors
 #' @param palette Character name of palette in gogh_palettes_pop
 #' @param discrete Boolean if color aesthetic is discrete
-#' @param reverse Boolean if palette should be reversed
+#' @param reverse Boolean if the palette should be reversed
 #' @param ... Additional arguments used to discrete_scale() or scale_fill_gradientn()
-#'   to automatically interpolate between colours.
-#' @return No return value. Called for side effects
-#' @examples
-#' if (require(ggplot2)) {
-#'   data <- data.frame(c = LETTERS[1:3],x = c(1,5,7),y = c(5,9,13))
-#'   ggplot(data, aes(x,fill=c))+geom_bar()+scale_fill_gogh()
-#' }
+#'   to automatically interpolate between colors.
 #' @export
+#' @return No return value. Called for side effects.
 scale_fill_gogh <- function(palette = "sunflowers", discrete = TRUE, reverse = FALSE, ...) {
   pal <- gogh_interpolate(palette = palette, reverse = reverse)
   if (discrete) {
-    # ggplot2::discrete_scale("fill", palette, palette = pal, ...)
     ggplot2::discrete_scale("fill", paste0("gogh_", palette), palette = pal, ...)
   } else {
     ggplot2::scale_fill_gradientn(colours = pal(256), ...)
   }
 }
 
+#' @examples
+#' if (require(ggplot2)) {
+#'   data <- data.frame(c = LETTERS[1:3],x = c(1,5,7),y = c(5,9,13))
+#'   ggplot(data, aes(x,y,color = c))+geom_point()+scale_color_gogh()
+#' }
+
+#' @examples
+#' if (require(ggplot2)) {
+#'   data <- data.frame(c = LETTERS[1:3],x = c(1,5,7),y = c(5,9,13))
+#'   ggplot(data, aes(x,fill=c))+geom_bar()+scale_fill_gogh()
+#' }
